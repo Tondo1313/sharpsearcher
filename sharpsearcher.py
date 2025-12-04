94b3b1da52abde819e775f833307fe0affeed838
Tondo1313 <aliss.kuper2@gmail.com>
2025-12-04 14:45:44 -0500

    Add files via upload

---
+++ sharpsearcher.py
@@ -0,0 +1,54 @@
+print(' ')
+print(' ')
+print('   ')
+print('     #                                               # ')
+print(' ##  # #   ##  #  ## # ##   ##   ##   ##  #  ##  ##  # #   ##  #  ##')
+print('#  # #  #    # # #   #   # #  # #  #    # # #   #    #  # #  # # #')
+print('#    #  #    # #     # ##  #    #  #    # #     #    #  # #  # #  ')
+print(' ##  #  #  # # #     #      ##  # ##  # # #     #    #  # # ## #')
+print('   # #  # #  # #     #        # #    #  # #     #    #  # #    #')
+print('#  # #  # #  # #     #     #  # #    #  # #     #    #  # #    #')
+print(' ##  #  #  ##  #     #      ##   ###  ### #      ##  #  #  ### #')
+print(' ')
+print('                                            by Tondo1313')
+print('      ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+import webbrowser
+import time
+
+user_text = input("write username of target without @: ").strip()
+
+url1 = f"https://www.tiktok.com/@{user_text}"
+url2 = f"https://www.instagram.com/{user_text}"
+url3 = f"https://www.facebook.com/{user_text}"
+url4 = f"https://www.youtube.com/@{user_text}"
+url5 = f"https://github.com/{user_text}"
+url6 = f"{user_text}"
+url7 = f"{user_text}"
+url8 = f"{user_text}"
+url9 = f"{user_text}"
+url10 = f"{user_text}"
+url11 = f"{user_text}"
+url12 = f"{user_text}"
+webbrowser.open(url1)
+time.sleep(1)
+webbrowser.open(url2)
+time.sleep(1)
+webbrowser.open(url3)
+time.sleep(1)
+webbrowser.open(url4)
+time.sleep(1)
+webbrowser.open(url5)
